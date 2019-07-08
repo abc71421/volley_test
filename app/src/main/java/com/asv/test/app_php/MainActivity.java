bfa1c60610961edf9c0153173ae6a15095e3f9ac
Assassin51243 <abc71421@gmail.com>
2019-07-09 02:44:36 +0800

    完成資料上傳

--- app/src/main/java/com/asv/test/app_php/MainActivity.java
+++ app/src/main/java/com/asv/test/app_php/MainActivity.java
@@ -6,7 +6,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -18,13 +20,18 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     private RequestQueue mQueue;
     private final static String mUrl = "http://192.168.1.6:80/AndroidConnectDB/conn.php";
+    private final static String mUrl_S = "http://192.168.1.6:80/AndroidConnectDB/get.php";
 
     private TextView msg;
     private Button mGetMsgBtn;
+    private Button mSendMsgBtn;
     private String TAG = "ONCONNECT";
 
     @Override
@@ -32,8 +39,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        msg = (TextView) findViewById(R.id.textView);
-        mGetMsgBtn = (Button) findViewById(R.id.button);
+        msg = findViewById(R.id.textView);
+        mGetMsgBtn =findViewById(R.id.button);
+        mSendMsgBtn =findViewById(R.id.button2);
         mQueue = Volley.newRequestQueue(getApplicationContext());
 
         mGetMsgBtn.setOnClickListener(new View.OnClickListener() {
@@ -41,6 +49,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 msg.setText("");
                 volley_JsonObjectRequestPOST();
+
+            }
+        });
+
+        mSendMsgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                msg.setText("");
+                volley_JsonObjectRequestPOST_Insert();
+                msg.setText("OK");
             }
         });
     }
@@ -56,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
         JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.POST, mUrl,null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                Log.d(TAG, "onResponse: " + response.toString());
+                Log.d(TAG, "onResponse: Check what the php will show ===>  " + response.toString());
                 try {
                     JSONArray data = response.getJSONArray("data");
                     for (int i = 0; i < data.length(); i++) {
@@ -78,5 +96,47 @@ public class MainActivity extends AppCompatActivity {
         });
         mQueue.add(getRequest);
     }
+
+
+    /***
+     * JsonObjectRequest
+     * JsonObjectRequest — To send and receive JSON Object from the Server
+     *
+     * POST方式請求伺服器
+     */
+    private void volley_JsonObjectRequestPOST_Insert(){
+        /**
+         * 用map.put來增加JSON內資料
+         *
+         * */
+        Map<String,String> map =new HashMap<String, String>();
+        map.put("id","10527088");
+        map.put("score","15");
+        map.put("name","WWWXD");
+        JSONObject data_send = new JSONObject(map);
+
+        JsonObjectRequest postRequest = new JsonObjectRequest(Request.Method.POST, mUrl_S,data_send, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, "onResponse: "+response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "onErrorResponse: "+error.toString());
+            }
+        })  {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String,String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json; charset=utf-8");
+                return headers;
+            }
+        };
+        postRequest.setTag("volley_JsonObjectRequestPost");
+        mQueue.add(postRequest);
+    }
+
+
 
 }
